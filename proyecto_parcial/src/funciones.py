de8774aa06f9485bf004a51b42e35a824fe7ed74
fecheverry-prog <federica.echeverry@gmail.com>
2026-05-15 12:42:12 +0000

    Agrego funciones y datos de registros

---
+++ proyecto_parcial/src/funciones.py
@@ -0,0 +1,2 @@
+def registro_usuarios(nombre, email = , telefono = , ciudad = ):
+    Función que permite guardar registro de los usuarios. Se guarda su nombre, email, teléfono y ciudad. El único dato obligatorio es el nombre, el resto puede estar incompleto.
